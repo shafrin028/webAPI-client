673b2b01c4ee76390c2550d9291f0f32091b2654
shafrin <shafrin@reberylit.com>
2022-05-05 21:13:22 +0530

    improvements aboutus and contact page

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,7 +1,18 @@
-
 const About = () => {
     return (
-        <h1>About</h1>
+        <>
+            <div className="bg-light">
+                <div className="container py-5">
+                    <div className="row h-100 align-items-center py-5">
+                    <div className="col-lg-6">
+                        <h1 className="display-4">About us page</h1>
+                        <p className="lead text-muted mb-0">This the about page of Adopt-A-Cat.</p>
+                    </div>
+                    <div className="col-lg-6 d-none d-lg-block"><img src="https://bootstrapious.com/i/snippets/sn-about/illus.png" alt="" className="img-fluid"/></div>
+                    </div>
+                </div>
+            </div>    
+        </>
     )
 }
 
